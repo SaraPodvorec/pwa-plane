9065b5a9ea98bc6fecafaab9412935d40f0132f0
SaraPodvorec <147993330+SaraPodvorec@users.noreply.github.com>
2026-02-01 23:01:47 +0100

    sw bug

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'PWA Plane App',
-        short_name: 'PWA Plane',
-        description: 'A Progressive Web App that enables users to control a plane using their voice',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          }
-        ]
-      },
-      workbox: {
-        cleanupOutdatedCaches: true
-      }
-    })
+    vue()
   ]
 })
